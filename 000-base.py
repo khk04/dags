fe886eef4df5fa1485c5aaf36b259dec42aec419
김현기 <khkraining@juxtagene.com>
2024-11-22 10:49:56 +0900

    BashOperator 및 KubernetesPodOperator 작업 추가

--- 000-base.py
+++ 000-base.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
 
 default_args = {
@@ -16,6 +18,20 @@ with DAG(
     catchup=False,
 ) as dag:
     start = DummyOperator(task_id='start')
+    print_date = BashOperator(
+        task_id='print_date',
+        bash_command='date'
+    )
+    run_in_pod = KubernetesPodOperator(
+        namespace='default',
+        image="ubuntu:20.04",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        name="run-in-pod",
+        task_id="run_in_pod",
+        get_logs=True,
+    )
     end = DummyOperator(task_id='end')
 
-    start >> end
+    start >> print_date >> run_in_pod >> end
